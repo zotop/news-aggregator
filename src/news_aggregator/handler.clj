2ee9814b43b668ae6abe331875d17dbda3e481ce
Joao Guerreiro <jmdguerreiro@gmail.com>
2016-02-09 15:43:30 +0100

    root path redirects to static index.html

--- src/news_aggregator/handler.clj
+++ src/news_aggregator/handler.clj
@@ -4,7 +4,7 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (ring.util.response/redirect "/index.html"))
   (route/not-found "Not Found"))
 
 (def app
