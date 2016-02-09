483b42b63e7fe700b6eab0c9316385ad0043e353
Joao Guerreiro <jmdguerreiro@gmail.com>
2016-02-09 23:46:39 +0100

    sample json response

--- src/news_aggregator/handler.clj
+++ src/news_aggregator/handler.clj
@@ -1,12 +1,19 @@
 (ns news-aggregator.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware.json :as middleware]
+            [ring.util.response :refer [response]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] (ring.util.response/content-type
                      (ring.util.response/resource-response "index.html" {:root "public"}) "text/html"))
+  (GET  "/api" [] (response [{:name "Widget 1"} {:name "Widget 2"}]))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (middleware/wrap-json-response)
+  )
+)
+
