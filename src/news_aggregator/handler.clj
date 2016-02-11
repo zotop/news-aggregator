f7e1b059d4c9de1b2aa1e397182b8be559a6b5a6
Joao Guerreiro <jmdguerreiro@gmail.com>
2016-02-11 12:55:29 +0100

    calling two news service api's; guardian and ny times

--- src/news_aggregator/handler.clj
+++ src/news_aggregator/handler.clj
@@ -7,36 +7,56 @@
             [ring.util.response :refer [response]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defn parseGuardianResult [[head & tail] finalResult]
+(defn callNewYorkTimesApi 
+	[searchTerm]
+		(let [resp1 (http/get (str "http://api.nytimes.com/svc/search/v2/articlesearch.json?sort=newest&api-key=sample-key&q=" searchTerm))]
+			(def results (:docs (:response (json/read-str (:body @resp1) :key-fn keyword))))
+			(parseNYTimesResult results [])
+		)
+)
+
+(defn parseNYTimesResult [[head & tail] finalResult]
 	(if (not-empty head)
 		
+		(let [elem {:title (:main (:headline head)) :url (:web_url head)}]
+			(parseNYTimesResult tail (conj finalResult elem))
+		)
+		
+		{:nytimes finalResult}
+	) 
+)
+
+(defn parseGuardianResult [[head & tail] finalResult]
+	(if (not-empty head)
+
 		(let [elem {:title (:webTitle head) :url (:webUrl head)}]
 			(parseGuardianResult tail (conj finalResult elem))
 		)
-		
+
 		{:guardian finalResult}
 	) 
 )
 
-(defn callApi 
+(defn callGuardianApi 
 	[searchTerm]
-	(if (not= searchTerm nil) 
-		(do
-			(let [resp1 (http/get (str "http://content.guardianapis.com/search?order-by=newest&api-key=test&q=" searchTerm))]
+		(let [resp1 (http/get (str "http://content.guardianapis.com/search?order-by=newest&api-key=test&q=" searchTerm))]
 			(def results (:results (:response (json/read-str (:body @resp1) :key-fn keyword))))
-			(response  (parseGuardianResult results []))
-			)
+			(parseGuardianResult results [])
 		)
-		(str "Query parameter 'searchTerm' needs to be specified")
+)
+
+(defn callApis [searchTerm]
+	(if (not= searchTerm nil) 
+	 (response (conj [] (callNewYorkTimesApi searchTerm) (callGuardianApi searchTerm)))
+	 (str "Query Parameter 'searchTerm' needs to be specified")
 	)
-	
 )
 
 (defroutes app-routes
   (GET "/" [] (ring.util.response/content-type
                      (ring.util.response/resource-response "index.html" {:root "public"}) "text/html"))
 
-  (GET "/api" {params :params} [] (callApi (get params :searchTerm)))
+  (GET "/api" {params :params} [] (callApis (get params :searchTerm)))
 
   (route/not-found "Not Found"))
 
